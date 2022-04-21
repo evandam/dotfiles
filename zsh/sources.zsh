ebe488321ea27553387534bc3f5d9e341767566b
Evan Van Dam <evan@northernlabs.ca>
2022-04-21 11:23:41 -0700

    wip

--- zsh/sources.zsh
+++ zsh/sources.zsh
@@ -6,3 +6,6 @@ compinit -d $ZSH_CACHE/zcompdump
 if [[ -f "/usr/local/bin/aws_zsh_completer.sh" ]]; then
   source "/usr/local/bin/aws_zsh_completer.sh"
 fi
+
+source <(/Users/evan/.asdf/shims/kubectl completion zsh)
+source <(/opt/homebrew/bin/helm completion zsh)
